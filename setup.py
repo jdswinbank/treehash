17813651e318579b659c2734a40a375b4a149237
John Swinbank <john@swinbank.org>
2015-09-26 12:41:25 -0400

    Release 1.0.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name="TreeHash",
-    version="1.0",
+    version="1.0.1",
     packages=['treehash'],
     scripts=['bin/treehash'],
     license="Simplified BSD",
